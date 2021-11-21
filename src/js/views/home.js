d773f31671e7e452f8e284e35fcaae4754c63e9d
Jhonny Mendoza <jhomen11@gmail.com>
2021-11-20 22:00:28 -0300

    Estilos del componente Home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,8 +7,35 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<h1>Start Wars</h1>
+		<div className="container home">
+			<h1>Start Wars Blog</h1>
+			<div className="card_home">
+				<div className="card_item">
+					<img src={require("../../img/banner_1.jpg")} />
+					<h3>Characters</h3>
+					<p>the characters of the star wars saga</p>
+					<Link className="card_botton" to={`/characters`}>
+						Ver mas...
+					</Link>
+				</div>
+				<div className="card_item">
+					<img src={require("../../img/banner_2.jpg")} />
+					<h3>Planets</h3>
+					<p>the characters of the star wars saga</p>
+					<Link className="card_botton" to={`/characters`}>
+						Ver mas...
+					</Link>
+				</div>
+				<div className="card_item">
+					<img src={require("../../img/banner_3.jpg")} />
+					<h3>Vehicles</h3>
+					<p>the characters of the star wars saga</p>
+					<Link className="card_botton" to={`/characters`}>
+						Ver mas...
+					</Link>
+				</div>
+			</div>
+			<div />
 		</div>
 	);
 };
